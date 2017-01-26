f468f805aaeda0a71e664e3d7da4256ee51bf86c
rewenset <a.zakurenyi@gmail.com>
2017-01-26 18:28:25 +0200

    rewrited with using requests and lxml libraries

--- companies_to_pickle.py
+++ companies_to_pickle.py
@@ -1,41 +1,66 @@
-from urllib.request import urlopen, Request
-
-import re
+from requests import Session
+from lxml import html
+from functools import partial
 
 from file_utils import save_with_pickle
-from company_parser import CompanyParser
+
+session = Session()
+session.headers.update({
+                         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) '
+                                       'Chrome/23.0.1271.64 Safari/537.11 '
+                     })
+my_request = partial(session.get, params="switch_lang=en")
+
+
+def get_elements_tree(url):
+    page = my_request(url)
+    tree = html.fromstring(page.content)
+    return tree
+
+
+def get_new_company(company):
+    """ Creates dict for new company and tries to find out
+        location of offices and company website url
+    """
+
+    tree = get_elements_tree(company['dou_url'])
+
+    offices = tree.xpath("//div[@class='offices']/text()") or ['']
+    site = tree.xpath("//div[@class='site']/a/@href") or ['']
+
+    new_company = {
+        'name': company['name'],
+        'offices': offices[0].strip() or 'not specified',
+        'site': site[0] or company['dou_url']
+    }
+
+    return new_company
 
 
 def companies_generator():
     """ Iterates through the list and returns dict with company site on 'dou.ua' and company name
     """
     url = 'https://jobs.dou.ua/ratings/'
+    tree = get_elements_tree(url)
+    elements = tree.xpath("//td[@class='company-name']/a")
 
-    req = Request(url)
-    resp = urlopen(req)
-    resp_data = resp.read()
-
-    text = str(resp_data)
-    pattern = r'<td class="company-name">\\n[\\(?=t)]*<a href="(?P<dou_url>.*?)">(?P<company_name>.*?)</a>\\n[\\(?=t)]+</td>'
-
-    for match in re.finditer(pattern, text):
-        yield {'dou_url': match.group('dou_url'), 'name': match.group('company_name')}
+    for element in elements:
+        yield get_new_company({'dou_url': element.xpath("@href")[0],
+                               'name': element.xpath("text()")[0]})
 
 
 def init_companies():
     """ Main function
     """
     companies = []
-    company_parser = CompanyParser()
     companies_gen = companies_generator()
 
     for counter, company in enumerate(companies_gen):
-        new_company = company_parser.get_new_company(company)
-        companies.append(new_company)
+        companies.append(company)
 
         print(counter)
         # if counter == 10:
-        #      break
+        #     break
 
     print('Number of companies: {}'.format(len(companies)))
 
--- html_page_gen.py
+++ html_page_gen.py
@@ -1,12 +1,12 @@
 from file_utils import load_with_pickle
-
+import codecs
 
 def generate_html_page():
     """ Generates html page 'companies.html' with list of companies
     """
     companies = load_with_pickle()
 
-    with open('companies.html', 'w') as file:
+    with codecs.open('companies.html', 'w', 'utf-8') as file:
 
         with open("template.html") as template:
             for line in template:
